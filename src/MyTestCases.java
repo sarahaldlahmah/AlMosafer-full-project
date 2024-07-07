11169ba313f52cc9f83dd19ed6ad9d2ccef7ad2e
sarahaldlahmah <sarah.aldlahmah@hotmail.com>
2024-07-07 23:11:06 +0300

    adding new test cases

--- src/MyTestCases.java
+++ src/MyTestCases.java
@@ -1,5 +1,6 @@
 import static org.testng.Assert.assertEquals;
 
+import java.nio.channels.Selector;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Random;
@@ -8,9 +9,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.By.ByClassName;
 import org.openqa.selenium.By.ByCssSelector;
 import org.openqa.selenium.By.ByTagName;
+import org.openqa.selenium.By.ByXPath;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -99,16 +102,80 @@ public void checkDepartureandReturnDates (){
 
 }
 
-@Test (priority = 7, enabled = true)
+@Test (priority = 7, enabled = false)
 public void checkLanguageIsSelectedRandomly () {
 	
+	String [] WebsiteLang = {"https://www.almosafer.com/ar" , "https://www.almosafer.com/en"};
+	int theRandomValue = rand.nextInt(WebsiteLang.length);
+	driver.get(WebsiteLang[theRandomValue]);
+	
+	String theExpectedARlangaues = "ar";
+	String theExpectedEnlangaues = "en";
+	
+	String theActualLanguage =  driver.findElement(By.tagName("html")).getAttribute("lang");
+	if (driver.getCurrentUrl().contains("en")) {
+		assertEquals(theActualLanguage, theExpectedEnlangaues); }
+		else { 
+			
+		assertEquals(theActualLanguage, theExpectedARlangaues);
+	}
+	
+	}
+
+@Test (priority = 8, enabled = true) 
+public void checkHotelsTab (){ 
+	
+    WebElement hotelsTab = driver.findElement(By.xpath("//a[@data-testid='Header__HotelsNavigationTab']")); 
+    hotelsTab.click();
+    WebElement searchBar = driver.findElement(By.cssSelector(".sc-phbroq-2.uQFRS.AutoComplete__Input")); // correct css selector
+	String [] citiesInEnglish = {"Dubai", "Jeddah", "Riyadh"};
+	String [] citiesInAr = {"جدة", "دبي"};
+	
+	if (driver.getCurrentUrl().contains("en")) {
+		int randomIndex = rand.nextInt(citiesInEnglish.length);
+		searchBar.sendKeys(citiesInEnglish[randomIndex]);
+		}
+	else {
+		
+		int randomIndex = rand.nextInt(citiesInAr.length);
+		searchBar.sendKeys(citiesInAr[randomIndex]);
+	} }
+		
+		
+@Test (priority = 9, enabled = true) 
+public void checkReservation () {
 	
 	
+	WebElement theReservationsOptions = driver.findElement(By.xpath("//select [@data-testid='HotelSearchBox__ReservationSelect_Select']"));
+	Select mySelector = new Select(theReservationsOptions);
+	int randomeIndex = rand.nextInt(2);
+	mySelector.selectByIndex(randomeIndex);
+}
+
+@Test (priority = 10, enabled = true)
+public void clickonSearch () {
+ WebElement theSearchButton = driver.findElement(By.cssSelector(".sc-jTzLTM.hQpNle.sc-1vkdpp9-6.iKBWgG.js-HotelSearchBox__SearchButton.btn.btn-primary.btn-block"));
+ theSearchButton.click();
 	
 	
 }
 
-}
+
+
+
+} // the class 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
